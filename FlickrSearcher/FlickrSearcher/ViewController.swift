dd54520a473d8d7efbd067fa6653595e247bec47
Shane Chi <schi@nxtcloud.com>
2020-01-24 23:39:16 +0800

    Create Xcode project

---
+++ FlickrSearcher/FlickrSearcher/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FlickrSearcher
+//
+//  Created by CHI on 2020/1/24.
+//  Copyright © 2020 CHI. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
