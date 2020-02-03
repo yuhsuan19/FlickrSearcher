1f853477c39bbdd02620c048a60be3d5634fcd1b
Shane Chi <schi@nxtcloud.com>
2020-02-03 13:37:01 +0800

    Add pod: CouchbaseLite Swift

--- FlickrSearcher/Pods/Target Support Files/Pods-FlickrSearcher/Pods-FlickrSearcher-frameworks.sh
+++ FlickrSearcher/Pods/Target Support Files/Pods-FlickrSearcher/Pods-FlickrSearcher-frameworks.sh
@@ -154,12 +154,16 @@ strip_invalid_archs() {
 
 if [[ "$CONFIGURATION" == "Debug" ]]; then
   install_framework "${BUILT_PRODUCTS_DIR}/Alamofire/Alamofire.framework"
+  install_framework "${PODS_ROOT}/CouchbaseLite-Swift/iOS/CouchbaseLiteSwift.framework"
+  install_dsym "${PODS_ROOT}/CouchbaseLite-Swift/iOS/CouchbaseLiteSwift.framework.dSYM"
   install_framework "${BUILT_PRODUCTS_DIR}/Kingfisher/Kingfisher.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/PKHUD/PKHUD.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/SwiftyJSON/SwiftyJSON.framework"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
   install_framework "${BUILT_PRODUCTS_DIR}/Alamofire/Alamofire.framework"
+  install_framework "${PODS_ROOT}/CouchbaseLite-Swift/iOS/CouchbaseLiteSwift.framework"
+  install_dsym "${PODS_ROOT}/CouchbaseLite-Swift/iOS/CouchbaseLiteSwift.framework.dSYM"
   install_framework "${BUILT_PRODUCTS_DIR}/Kingfisher/Kingfisher.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/PKHUD/PKHUD.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/SwiftyJSON/SwiftyJSON.framework"
